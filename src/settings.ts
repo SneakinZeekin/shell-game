55a7662d04ca244da3bd589e83bc792801cfb135
SneakinZeekin <SneakinZeekin@gmail.com>
2025-11-26 19:05:12 -0500

    changed slider option to number boxes

--- src/settings.ts
+++ src/settings.ts
@@ -1,33 +1,37 @@
 export const MODULE_ID = "shell-game";
 
 export function registerSettings() {
-  game.settings.register(MODULE_ID, "runtimeMs", {
-    name: "Shell Game: Run Time (ms)",
-    hint: "Total duration of the shuffle animation, in milliseconds.",
-    scope: "world",
-    config: true,
-    type: Number,
-    default: 8000,
-    range: { min: 1000, max: 60000, step: 500 }
-  });
+    game.settings.register(MODULE_ID, "runtimeMs", {
+        name: "Shell Game: Run Time (ms)",
+        hint: "Total duration of the shuffle animation, in milliseconds.",
+        scope: "world",
+        config: true,
+        type: Number,
+        default: 8000,
+        integer: true,
+        range: { min: 1000, max: 60000 }
+    });
 
-  game.settings.register(MODULE_ID, "minMoveMs", {
-    name: "Shell Game: Minimum Move Duration (ms)",
-    hint: "Minimum duration of each hop animation, in milliseconds.",
-    scope: "world",
-    config: true,
-    type: Number,
-    default: 350,
-    range: { min: 50, max: 5000, step: 50 }
-  });
+    game.settings.register(MODULE_ID, "minMoveMs", {
+        name: "Shell Game: Minimum Move Duration (ms)",
+        hint: "Minimum duration of each move animation, in milliseconds.",
+        scope: "world",
+        config: true,
+        type: Number,
+        default: 350,
+        integer: true,
+        range: { min: 50, max: 5000 }
+    });
 
-  game.settings.register(MODULE_ID, "maxMoveMs", {
-    name: "Shell Game: Maximum Move Duration (ms)",
-    hint: "Maximum duration of each hop animation, in milliseconds.",
-    scope: "world",
-    config: true,
-    type: Number,
-    default: 350,
-    range: { min: 50, max: 5000, step: 50 }
-  });
+    game.settings.register(MODULE_ID, "maxMoveMs", {
+        name: "Shell Game: Maximum Move Duration (ms)",
+        hint: "Maximum duration of each move animation, in milliseconds.",
+        scope: "world",
+        config: true,
+        type: Number,
+        default: 400,
+        integer: true,
+        range: { min: 50, max: 5000 }
+    });
+
 }
