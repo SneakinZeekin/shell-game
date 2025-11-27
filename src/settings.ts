6ca90ce68908d9fdf785635cbb918533bf8638e4
SneakinZeekin <SneakinZeekin@gmail.com>
2025-11-26 19:23:18 -0500

    back to sliders with a step of 50

--- src/settings.ts
+++ src/settings.ts
@@ -9,7 +9,7 @@ export function registerSettings() {
         type: Number,
         default: 8000,
         integer: true,
-        range: { min: 1000, max: 60000 }
+        range: { min: 1000, max: 60000, step: 50 }
     });
 
     game.settings.register(MODULE_ID, "minMoveMs", {
@@ -20,7 +20,7 @@ export function registerSettings() {
         type: Number,
         default: 350,
         integer: true,
-        range: { min: 50, max: 5000 }
+        range: { min: 50, max: 5000, step: 50 }
     });
 
     game.settings.register(MODULE_ID, "maxMoveMs", {
@@ -31,7 +31,7 @@ export function registerSettings() {
         type: Number,
         default: 400,
         integer: true,
-        range: { min: 50, max: 5000 }
+        range: { min: 50, max: 5000, step: 50 }
     });
 
 }
